bb480512f38a152331622744403b566dac26cac4
Jhon Vallejo <jvallejos2@unemi.edu.ec>
2023-01-19 00:19:57 -0500

    Creacion del modulo de reserva

--- ven/Scripts/activate.bat
+++ ven/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Usuario\Documents\Trabajos\Sistema de reservas\ven
+set VIRTUAL_ENV=C:\Users\erick\Erick\Proyectos\Proyecto de complejo deportivo\Modulo-de-reservas-para-un-complejo-deportivo-usando-codigo-QR\ven
 
 if not defined PROMPT set PROMPT=$P$G
 
