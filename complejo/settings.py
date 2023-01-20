2acfb995f5ede2a3054599cb88fdf4ac2a4a561b
ariel torres <atorresm@unemi.edu.ec>
2023-01-19 22:41:51 -0500

    conexion base de datos

--- complejo/settings.py
+++ complejo/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'complejo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_reserva', 
+        'HOST': 'localhost',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'PORT': '5432',
     }
 }
 
